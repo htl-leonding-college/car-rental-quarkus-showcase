63596e663fe73c18f16bce3bb8d380401f04baa6
htl-leonding <t.stuetz@htl-leonding.ac.at>
2020-10-21 15:22:50 +0200

    initial commit

---
+++ src/test/java/at/htl/vehicle/boundary/NativeVehicleResourceIT.java
@@ -0,0 +1,11 @@
+package at.htl.vehicle.boundary;
+
+import io.quarkus.test.junit.NativeImageTest;
+import org.junit.jupiter.api.Disabled;
+
+@Disabled
+@NativeImageTest
+public class NativeVehicleResourceIT extends VehicleResourceTest {
+
+    // Execute the same tests but in native mode.
+}
