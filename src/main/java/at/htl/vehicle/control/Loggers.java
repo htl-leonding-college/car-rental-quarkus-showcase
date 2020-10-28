66004705bf479ae9d0e3a3e75b4496f8a46a092a
htl-leonding <t.stuetz@htl-leonding.ac.at>
2020-10-28 09:58:33 +0100

    add option 'canonical name' to Logger

--- src/main/java/at/htl/vehicle/control/Loggers.java
+++ src/main/java/at/htl/vehicle/control/Loggers.java
@@ -14,6 +14,7 @@ public class Loggers {
                         .getMember()
                         .getDeclaringClass()
                         .getSimpleName()
+                        //.getCanonicalName()
                 );
     }
 }
